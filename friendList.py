5bcc0304a2765a4976e356130bfe2404c71bb723
sol lee <76844556+soleu@users.noreply.github.com>
2021-03-27 23:26:16 +0900

    Add files via upload

---
+++ friendList.py
@@ -0,0 +1,14 @@
+flist=[]
+f1=input("친구의 이름을 입력하시오:")
+f2=input("친구의 이름을 입력하시오:")
+f3=input("친구의 이름을 입력하시오:")
+f4=input("친구의 이름을 입력하시오:")
+f5=input("친구의 이름을 입력하시오:")
+
+flist.append(f1)
+flist.append(f2)
+flist.append(f3)
+flist.append(f4)
+flist.append(f5)
+
+print(flist)
